7cdaafeee5fa10a1969a7d47a21926dc24ca9e22
Carter Chavez <carter.chavez@ufl.edu>
2022-10-24 18:37:27 -0400

    added user input

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+
+a = int(input("Enter first number:"))
+b = int(input("Enter Second number:"))
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+print(f"adding both numbers:{add(a , b)}")
+print(f"Subtracting both numbers:{subtract(a, b)}")
